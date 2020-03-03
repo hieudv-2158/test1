d149befc7fd054f9bb6e104ab656b0350cacf962
dinh.van.hieu <SUN-ASTERISK\dinh.van.hieu@b122158.sun-asterisk.com>
2020-03-03 10:30:25 +0700

    commit1

--- test/app.test.js
+++ test/app.test.js
@@ -14,6 +14,8 @@ const getUrl = pathname => url.format({
 describe('Feathers application tests', () => {
   let server;
 
+
+  
   before(function(done) {
     server = app.listen(port);
     server.once('listening', () => done());
